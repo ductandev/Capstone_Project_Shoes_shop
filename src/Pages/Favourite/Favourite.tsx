74571d3ca5db44d8475f72c994d4437028defb8d
Jonathan <nvthanh.fedev.0801@Gmail.com>
2023-06-02 01:01:18 +0700

    thanh update header

---
+++ src/Pages/Favourite/Favourite.tsx
@@ -0,0 +1,8 @@
+// tsrfc
+import React from "react";
+
+type Props = {};
+
+export default function Favourite({}: Props) {
+  return <div>Favourite</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { unstable_HistoryRouter as HistoryRouter, Routes, Route, Navigate } from 'react-router-dom'
+import {
+  unstable_HistoryRouter as HistoryRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-import { createBrowserHistory } from 'history'
-import HomeTemplate from './Templates/HomeTemplate';
-import Home from './Pages/Home/Home';
-import Detail from './Pages/Detail/Detail';
-import Search from 'antd/es/transfer/search';
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register';
-import Cart from './Pages/Cart/Cart';
-import Profile from './Pages/Profile/Profile';
+import { createBrowserHistory } from "history";
+import HomeTemplate from "./Templates/HomeTemplate";
+import Home from "./Pages/Home/Home";
+import Detail from "./Pages/Detail/Detail";
+import Search from "antd/es/transfer/search";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
+import Cart from "./Pages/Cart/Cart";
+import Profile from "./Pages/Profile/Profile";
 //Setup redux
-import { Provider } from 'react-redux'
-import { store } from './Redux/configStore';
-import Loading from './Components/Loading/Loading';
-
+import { Provider } from "react-redux";
+import { store } from "./Redux/configStore";
+import Loading from "./Components/Loading/Loading";
+import "./globalStyles.css";
 
 export const history: any = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
     {/* <Loading></Loading> */}
     <HistoryRouter history={history}>
       <Routes>
-        <Route path='' element={<HomeTemplate />}>
+        <Route path="" element={<HomeTemplate />}>
           <Route index element={<Home />}></Route>
           <Route path="detail">
-            <Route path=':id' element={<Detail />}></Route>
+            <Route path=":id" element={<Detail />}></Route>
           </Route>
           <Route path="search" element={<Search />}></Route>
           <Route path="login" element={<Login />}></Route>
